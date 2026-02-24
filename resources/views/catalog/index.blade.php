f03a0c66ef83add4c8e619e1a3b64691054e1f66
Derek Murillo Fernandez <derek.murillo@saint-gobain.com>
2026-02-24 08:27:17 +0100

    PT2

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,5 +1,37 @@
-@extends('layouts.master')
+<div class="row">
+    @foreach ($arrayPeliculas as $key => $pelicula)
+        <div class="col-xs-6 col-sm-4 col-md-3 text-center"> <a href="{{ route('catalog.show', $key) }}"> <img
+                    src="{{ $pelicula['poster'] }}" style="height:200px" />
+                <h4 style="min-height: 45px; margin:5px 0 10px 0"> {{ $pelicula['title'] }} </h4>
+            </a> </div>
+    @endforeach
+</div>
 
-@section('content')
-    <h1 class="text-2xl font-bold">Listado de películas</h1>
-@endsection
+<div class="row">
+    <div class="col-sm-4">
+        <img src="{{ $pelicula['poster'] }}" style="width:100%" alt="Póster de la película">
+    </div>
+    <div class="col-sm-8">
+        <h2>{{ $pelicula['title'] }}</h2>
+        <h4>Año: {{ $pelicula['year'] }}</h4>
+        <h4>Director: {{ $pelicula['director'] }}</h4>
+        <p><strong>Resumen:</strong> {{ $pelicula['synopsis'] }}</p>
+
+        <p><strong>Estado: </strong>
+            @if ($pelicula['rented'])
+                Película actualmente alquilada.
+            @else
+                Película disponible.
+            @endif
+        </p>
+
+        @if ($pelicula['rented'])
+            <button class="btn btn-danger">Devolver película</button>
+        @else
+            <button class="btn btn-primary">Alquilar película</button>
+        @endif
+
+        <a class="btn btn-warning" href="{{ route('catalog.edit', $id) }}">Editar película</a> <a
+            class="btn btn-default btn-outline-dark" href="{{ route('catalog') }}">Volver al listado</a>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,15 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('inici');
+Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog');
+Route::get('/catalog/show/{id}', [CatalogController::class, 'show'])->name('catalog.show');
+Route::get('/catalog/create', [CatalogController::class, 'create'])->name('catalog.create');
+Route::get('/catalog/edit/{id}', [CatalogController::class, 'edit'])->name('catalog.edit');
 
 Route::get('login', function () {
     return view('auth.login');
-});
-
-Route::get('catalog', function () {
-    return view('catalog.index');
-});
-
-Route::get('catalog/show/{id}', function ($id) {
-    return view('catalog.show', array('id' => $id));
-});
-
-Route::get('catalog/create', function () {
-    return view('catalog.create');
-});
-
-Route::get('catalog/edit/{id}', function ($id) {
-    return view('catalog.edit', array('id' => $id));
 });
